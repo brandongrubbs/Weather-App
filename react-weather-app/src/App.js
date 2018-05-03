664dd34740c7855cb90a791473fb0f61c95406b2
Brandon Grubbs 1 <unique2199@aol.com>
2018-05-03 17:50:31 -0400

    app upload

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "800c73806df99c78540f32b6f0fa2321";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    lo: undefined,
+    high: undefined,
+    error: undefined
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const data = await api_call.json();
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        lo: data.main.temp_min,
+        high: data.main.temp_max,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        lo: undefined,
+        high: undefined,
+        error: "Please enter city and two letter country code."
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather 
+                    temperature={this.state.temperature} 
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    lo={this.state.lo }
+                    high={this.state.high}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ react-weather-app/src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">My Weather</h1>
+		<h3 className="title-container__subtitle">Rain, sleet, hail, or snow, with this app you will know</h3>
+	</div>
+);
+
+export default Titles;
